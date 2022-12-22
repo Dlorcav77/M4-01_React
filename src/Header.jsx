71cd83c8b3044a5b905006cbfd43aa94141dfc6e
Diego_Lorca <diego.lorcaveliz@gmail.com>
2022-12-22 13:13:04 -0300

    02-react

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 const Header = ({title}) => {
     return (
-        <h1 className="text-center">{title}</h1>
+        <h1 className="text-center bg-dark p-2 m-0">{title}</h1>
     );
 };
 export default Header;
